caac47f2a37dd129eef7c5c5caffaed0812788bc
kagamaru <01a17124@gmail.com>
2025-01-13 15:05:47 +0900

    :wrench: eslintとprettierの導入

--- handler.js
+++ handler.js
@@ -1,9 +1,9 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
 const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
+    DynamoDBDocumentClient,
+    GetCommand,
+    PutCommand,
 } = require("@aws-sdk/lib-dynamodb");
 
 const express = require("express");
@@ -18,57 +18,57 @@ const docClient = DynamoDBDocumentClient.from(client);
 app.use(express.json());
 
 app.get("/users/:userId", async (req, res) => {
-  const params = {
-    TableName: USERS_TABLE,
-    Key: {
-      userId: req.params.userId,
-    },
-  };
+    const params = {
+        TableName: USERS_TABLE,
+        Key: {
+            userId: req.params.userId,
+        },
+    };
 
-  try {
-    const command = new GetCommand(params);
-    const { Item } = await docClient.send(command);
-    if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
-    } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
+    try {
+        const command = new GetCommand(params);
+        const { Item } = await docClient.send(command);
+        if (Item) {
+            const { userId, name } = Item;
+            res.json({ userId, name });
+        } else {
+            res.status(404).json({
+                error: 'Could not find user with provided "userId"',
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Could not retrieve user" });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
-  }
 });
 
 app.post("/users", async (req, res) => {
-  const { userId, name } = req.body;
-  if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
+    const { userId, name } = req.body;
+    if (typeof userId !== "string") {
+        res.status(400).json({ error: '"userId" must be a string' });
+    } else if (typeof name !== "string") {
+        res.status(400).json({ error: '"name" must be a string' });
+    }
 
-  const params = {
-    TableName: USERS_TABLE,
-    Item: { userId, name },
-  };
+    const params = {
+        TableName: USERS_TABLE,
+        Item: { userId, name },
+    };
 
-  try {
-    const command = new PutCommand(params);
-    await docClient.send(command);
-    res.json({ userId, name });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Could not create user" });
-  }
+    try {
+        const command = new PutCommand(params);
+        await docClient.send(command);
+        res.json({ userId, name });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Could not create user" });
+    }
 });
 
 app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
+    return res.status(404).json({
+        error: "Not Found",
+    });
 });
 
 exports.handler = serverless(app);

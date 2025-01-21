4fc8822bf8e5e6db5086bedfbdbb72b53916467a
kagamaru <01a17124@gmail.com>
2025-01-21 23:07:11 +0900

    :bug: 環境変数を正しく規定する

--- utility/getRDSDBClient.ts
+++ utility/getRDSDBClient.ts
@@ -1,12 +1,13 @@
+import { dbConfig } from "@/config";
 import { ServerlessMysql } from "serverless-mysql";
 
 export const getRDSDBClient = (): ServerlessMysql => {
     const mysqlClient = require("serverless-mysql")({
         config: {
-            host: process.env.ENDPOINT,
-            database: process.env.DATABASE,
-            user: process.env.USERNAME,
-            password: process.env.PASSWORD,
+            host: dbConfig.DB_HOST,
+            database: dbConfig.DB_NAME,
+            user: dbConfig.DB_USER,
+            password: dbConfig.DB_PASSWORD,
         },
     });
 

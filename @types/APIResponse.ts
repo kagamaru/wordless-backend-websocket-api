8285242bdd1c1d0f3c54ebf8a7a27e26142f9dc4
kagamaru <01a17124@gmail.com>
2025-01-26 14:39:37 +0900

    :sparkles: disconnectしたときのメソッド実装

--- @types/APIResponse.ts
+++ @types/APIResponse.ts
@@ -1,6 +1,6 @@
 export type APIResponse<T> = {
     statusCode: 200 | 400 | 500;
-    body:
+    body?:
         | T
         | {
               error: `${Uppercase<string>}${Uppercase<string>}${Uppercase<string>}-${0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}${0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}`;
--- app/ondisconnect/handler.ts
+++ app/ondisconnect/handler.ts
@@ -1,1 +1,56 @@
-export const disconnect = async (event: any) => {};
+import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { envConfig } from "@/config";
+import { APIResponse } from "@/@types";
+import { getDynamoDBClient } from "@/utility";
+
+const docClient = getDynamoDBClient();
+
+type DisConnectRequest = {
+    body: {
+        connectionId: string;
+    };
+};
+
+export const disconnect = async (
+    event: DisConnectRequest,
+): Promise<APIResponse<undefined>> => {
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: {
+                error: "EMT-91",
+            },
+        };
+    }
+    const { connectionId } = event.body;
+    if (!connectionId) {
+        return {
+            statusCode: 400,
+            body: {
+                error: "EMT-91",
+            },
+        };
+    }
+
+    try {
+        await docClient.send(
+            new DeleteCommand({
+                TableName: envConfig.USER_CONNECTION_TABLE,
+                Key: {
+                    connectionId,
+                },
+            }),
+        );
+
+        return {
+            statusCode: 200,
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: {
+                error: "EMT-92",
+            },
+        };
+    }
+};

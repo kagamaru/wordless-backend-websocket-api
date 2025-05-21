10db956f5050e86fac7e145672b62c1092740391
kagamaru <01a17124@gmail.com>
2025-05-21 22:22:52 +0900

    ✨ リクエストの認証処理を実態に合わせて修正

--- @types/index.ts
+++ @types/index.ts
@@ -1,5 +1,6 @@
 export * from "@/@types/APIRequest";
 export * from "@/@types/APIResponse";
+export * from "@/@types/ConnectAPIRequest";
 export * from "@/@types/FetchedEmoteReaction";
 export * from "@/@types/FetchedJWKKeys";
 export * from "@/@types/FetchedUserConnection";
